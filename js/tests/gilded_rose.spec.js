2eaa49663231456ce1131eeae04e32cb25d5a69e
Alexandra Marin <alexandra.marin01@gmail.com>
2018-10-11 22:30:42 +0200

    refactor passes

--- js/tests/gilded_rose.spec.js
+++ js/tests/gilded_rose.spec.js
@@ -86,66 +86,6 @@ describe('Gilded Rose', () => {
     });
 
     describe('Backstage passes', () => {
-        it('Fairly rare backstage passes with quality at most 48 increase by 2', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 48)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Fairly rare backstage passes with quality 49 increase by 1', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Fairly rare backstage passes with quality 50 remain constant', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 50)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Fairly common backstage passes with quality at most 48 increase by 1', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 48)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(49);
-        });
-
-        it('Fairly common backstage passes with quality 49 increase by 1', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 49)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Fairly common backstage passes with quality 50 remains constant', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 50)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Super rare backstage passes with quality at most 47 increase by 3', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 47)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Super rare backstage passes with quality 48 increase by 2', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 48)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Super rare backstage passes with quality 49 increase by 1', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
-        it('Super rare backstage passes with quality 50 remain constant', () => {
-            const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 50)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].quality).toEqual(50);
-        });
-
         it('Backstage passes with sellIn 0 have quality reduced to 0', () => {
             const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 30)]);
             const items = gildedRose.updateQuality();
@@ -156,6 +96,72 @@ describe('Gilded Rose', () => {
             const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 1, -3)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toEqual(0);
+        });
+
+        describe('Fairly common backstage passes', () => {
+            it('with quality at most 48 increase by 1', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 48)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(49);
+            });
+
+            it('with quality 49 increase by 1', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 49)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+
+            it('with quality 50 remains constant', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 50)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+        });
+
+        describe('Fairly rare backstage passes', () => {
+            it('with quality at most 48 increase by 2', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 48)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+
+            it('with quality 49 increase by 1', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+
+            it('with quality 50 remain constant', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 50)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+        });
+
+        describe('Super rare backstage passes', () => {
+            it('with quality at most 47 increase by 3', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 47)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+
+            it('with quality 48 increase by 2', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 48)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+
+            it('with quality 49 increase by 1', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
+
+            it('with quality 50 remain constant', () => {
+                const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 50)]);
+                const items = gildedRose.updateQuality();
+                expect(items[0].quality).toEqual(50);
+            });
         });
     });
 });
