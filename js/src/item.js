db668b411f2b17facd099af73d958af05a1cddc5
Alexandra Marin <alexandra.marin01@gmail.com>
2018-10-29 17:44:15 +0100

    default item

--- js/src/item.js
+++ js/src/item.js
@@ -4,4 +4,14 @@ export default class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
+
+    updateQuality = () => {
+        if (this.quality > 0) {
+            this.quality = this.quality - 1;
+        }
+        this.sellIn = this.sellIn - 1;
+        if (this.sellIn < 0 && this.quality > 0) {
+            this.quality = this.quality - 1;
+        }
+    }
 }
